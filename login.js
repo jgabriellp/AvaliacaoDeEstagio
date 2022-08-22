c511b94a52b30d110da9d4df0b13bc5538048fcb
Gabriel Lima <106336816+jgabriellp@users.noreply.github.com>
2022-08-22 11:40:54 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,22 @@
 var db = openDatabase("MeuBanco", "2.0", "MyBase", 4048)
-db.transaction(function(criar) {
-	criar.executeSql("CREATE TABLE users (ID PRYMARY KEY, email TEXT, senha TEXT)")
-})
+//db.transaction(function(criar) {
+//	criar.executeSql("CREATE TABLE users (ID PRYMARY KEY, email TEXT, senha TEXT)")
+//})
 
 function capturar() {
 	let email = document.getElementById("email").value
 	let senha = document.getElementById("senha").value
+	console.log(email)
+	console.log(senha)
+
+	var r = []
+
+	var sql = "SELECT email FROM users"
+	console.log(sql)
 
 	db.transaction(function(login) {
-		login.executeSql("SELECT * FROM users")
-		
+		let lo = login.executeSql(sql)
+		console.log(lo)
 	})
+
 }
