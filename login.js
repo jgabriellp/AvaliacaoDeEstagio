1c5a2f7e6369de7aaf0c2b5099d8af3506c2c971
Gabriel Lima <106336816+jgabriellp@users.noreply.github.com>
2022-08-23 14:35:56 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,19 @@
 var db = openDatabase("MeuBanco", "2.0", "MyBase", 4048)
-//db.transaction(function(criar) {
-//	criar.executeSql("CREATE TABLE users (ID PRYMARY KEY, email TEXT, senha TEXT)")
-//})
+db.transaction(function(criar) {
+	criar.executeSql("CREATE TABLE users (ID PRYMARY KEY, email TEXT, senha TEXT)")
+})
+
+function teste(x) {
+	var string = ""
+
+	if (x.length > 0) {
+		string = "deu tudo certo"
+		console.log(string)
+	} else {
+		string = "email não cadastrado"
+		console.log(string)
+	}
+}
 
 function capturar() {
 	let email = document.getElementById("email").value
@@ -9,18 +21,9 @@ function capturar() {
 	console.log(email)
 	console.log(senha)
 
-	var r = []
-
-	var sql = "SELECT email FROM users"
-	console.log(sql)
-
 	db.transaction(t => t.executeSql(
-		'SELECT u.email FROM users u ', [], (t, result) => console.log(result.rows)
-	))
-
-	//db.transaction(function(login) {
-	//	let lo = login.executeSql(sql)
-	//	console.log(lo)
-	//})
+		"select email from users where email='" + email + "'", [], (t, result) => teste(result.rows)
+		)
+	)
 
 }
