4f3219148d22ce445dc27ee226030d7e58e1f7ce
Gabriel Lima <106336816+jgabriellp@users.noreply.github.com>
2022-08-23 15:21:18 -0300

    Update login.js

--- login.js
+++ login.js
@@ -3,27 +3,53 @@ db.transaction(function(criar) {
 	criar.executeSql("CREATE TABLE users (ID PRYMARY KEY, email TEXT, senha TEXT)")
 })
 
-function teste(x) {
-	var string = ""
+function testaEmail(x) {
 
 	if (x.length > 0) {
-		string = "deu tudo certo"
-		console.log(string)
+		login = "deu tudo certo"
+		console.log(login)
 	} else {
-		string = "email não cadastrado"
-		console.log(string)
+		login = "email não cadastrado"
+		console.log(login)
+	}
+}
+
+function testaSenha(x) {
+
+	if (x.length > 0) {
+		key = "deu tudo certo"
+		console.log(key)
+	} else {
+		key = "senha incorreta"
+		console.log(key)
 	}
 }
 
 function capturar() {
 	let email = document.getElementById("email").value
 	let senha = document.getElementById("senha").value
-	console.log(email)
-	console.log(senha)
+
+	var login = ""
+	var key = ""
 
 	db.transaction(t => t.executeSql(
-		"select email from users where email='" + email + "'", [], (t, result) => teste(result.rows)
+		"select email from users where email='" + email + "'", [], (t, result) => testaEmail(result.rows)
 		)
 	)
+
+	db.transaction(s => s.executeSql(
+		"select senha from users where senha='" + senha + "'", [], (s, resultS) => testaSenha(resultS.rows)
+		)
+	)
+
+	if ((login == "deu tudo certo") && (key == "deu tudo certo")) {
+		console.log("você logou com sucesso")
+	} else if ((login == "deu tudo certo") && (key != "deu tudo certo")) {
+		console.log(key)
+	}else if ((login != "deu tudo certo") && (key == "deu tudo certo")) {
+		console.log(login)
+	} else {
+		console.log("faça seu cadastro")
+	}
 
 }
